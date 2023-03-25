05763dcd705876d0bab06f6201bdcc95a8aedbfa
Tokiwa-17 <2872444509@qq.com>
2023-03-25 17:16:22 +0800

    geometric method for finding the intersection of a ray and a sphere.

--- ray_casting/code/include/sphere.hpp
+++ ray_casting/code/include/sphere.hpp
@@ -13,19 +13,34 @@ public:
         // unit ball at the center
     }
 
-    Sphere(const Vector3f &center, float radius, Material *material) : Object3D(material) {
-        // 
+    Sphere(const Vector3f &center, float radius, Material *material) : Object3D(material), center(center), radius(radius) {
     }
 
     ~Sphere() override = default;
 
     bool intersect(const Ray &r, Hit &h, float tmin) override {
-        //
-        return false;
+        /**
+         \brief Geometric method for finding the intersection of ray and spheres.
+        */
+        Vector3f l = this -> center - r.getOrigin();
+        bool inSphere = Vector3f::dot(l, l) < this -> radius;
+        float projPoint = Vector3f::dot(l, r.getDirection()) / r.getDirection().length();
+        if (projPoint < 0)
+            return false;
+        float dSquare = Vector3f::dot(l, l) - projPoint * projPoint;
+        if (dSquare > radius * radius) 
+            return false;
+        float tSquare = radius * radius - dSquare;
+        float t = projPoint - sqrt(tSquare) ? inSphere : projPoint + sqrt(tSquare);
+        if (t > h.getT() || t < tmin);
+        Vector3f normal = r.pointAtParameter(t) - this -> center;
+        h.set(t, this->material, normal.normalized());
+        return true;
     }
 
 protected:
-
+    Vector3f center;
+    float radius;
 };
 
 
--- ray_casting/code/src/main.cpp
+++ ray_casting/code/src/main.cpp
@@ -33,6 +33,30 @@ int main(int argc, char *argv[]) {
     // through that pixel and finding its intersection with
     // the scene.  Write the color at the intersection to that
     // pixel in your output image.
+    SceneParser sceneParser(argv[1]);
+    auto camera = sceneParser.getCamera();
+    int width = camera -> getWidth();
+    int height = camera -> getHeight();
+    // for (int x = 0; x < width; ++x) {
+    //     for (int y = 0; y < height; ++y) {
+    //         Ray camRay = sceneParser.getCamera() -> generateRay(Vector2f(x, y));
+    //         Group* baseGroup = sceneParser.getGroup();
+    //         Hit hit;
+    //         bool isIntersect = baseGroup -> intersect(camRay, hit, 0);
+    //         if (isIntersect) {
+    //             Vector3f finalColor = Vector3f::ZERO;
+    //             for (int lightIdx = 0; lightIdx < sceneParser.getNumLights(); ++lightIdx) {
+    //                 Light * light = sceneParser.getLight(lightIdx);
+    //                 Vector3f L, lightColor;
+    //                 light -> getIllumination(camRay.pointAtParameter(hit.getT()), L, lightColor);
+    //                 finalColor += hit.getMaterial() -> Shade(camRay, hit, L, lightColor);
+    //             }
+    //             setPixel(x, y, finalColor);
+    //         } else {
+    //             setPixel(x, y, sceneParser.getBackgroundColor());
+    //         }
+    //     }
+    // }
     cout << "Hello! Computer Graphics!" << endl;
     return 0;
 }
