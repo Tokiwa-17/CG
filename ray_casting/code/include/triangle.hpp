758b0527e84be58fdcebf0b1502c645894a3a2c4
Tokiwa-17 <2872444509@qq.com>
2023-03-25 17:43:38 +0800

    compute intersection between a ray and a triangle

--- ray_casting/code/include/triangle.hpp
+++ ray_casting/code/include/triangle.hpp
@@ -15,16 +15,39 @@ public:
 
     // a b c are three vertex positions of the triangle
 	Triangle( const Vector3f& a, const Vector3f& b, const Vector3f& c, Material* m) : Object3D(m) {
-
+		vertices[0] = a;
+		vertices[1] = b;
+		vertices[2] = c;
+		auto t1 = b - a, t2 = c - a;
+		this -> normal = Vector3f::cross(t1, t2).normalized();
 	}
 
 	bool intersect( const Ray& ray,  Hit& hit , float tmin) override {
-        return false;
+		/**
+		 \brief computer intersection between a triangle and a ray.
+		*/
+		auto ori = ray.getOrigin();
+		auto dir = ray.getDirection().normalized();
+		auto s = ori - vertices[0];
+		auto e1 = vertices[1] - vertices[0];
+		auto e2 = vertices[2] - vertices[0];
+		auto s1 = Vector3f::cross(dir, e2);
+		auto s2 = Vector3f::cross(s, e1);
+		auto deno = 1 / (Vector3f::dot(s1, e1));
+		auto t = deno * Vector3f::dot(s2, e2);
+		auto beta = deno * Vector3f::dot(s1, s);
+		auto gamma = deno * Vector3f::dot(s2, dir);
+		auto alpha = 1 - beta - gamma;
+		bool valid = alpha >= 0 && alpha <= 1 && beta >= 0 && beta <= 1 && gamma >= 0 && gamma <= 1;
+		if (valid && t < hit.getT() && t > tmin) {
+			hit.set(t, this -> material, this -> normal);
+			return true;
+		} 
+		else return false;
 	}
 	Vector3f normal;
 	Vector3f vertices[3];
 protected:
-
 };
 
 #endif //TRIANGLE_H
